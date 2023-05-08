a19446c7f2705dd4e251305c58f086a1d79be24c
chauhan <samratchauhan.dev.gmail.com>
2023-05-08 23:58:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {data} from "./wineStats/data"
+import FlavanoidsStats from './components/FlavanoidsStats';
+import GammaStatsTable from './components/GammaStatsTable';
+
+
+function App() {
+
+
+  return (
+    <React.Fragment>
+      Wine Stats
+      <FlavanoidsStats data={data}/>
+      <GammaStatsTable data={data}/>
+    </React.Fragment>
+  )
+}
+
+export default App
